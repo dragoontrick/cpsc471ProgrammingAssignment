e26eab8e0bc482c139ab63852e284bbc0b3fde1c
dragoontrick <47570619+dragoontrick@users.noreply.github.com>
2023-10-31 21:15:23 -0700

    translation of profs help

--- socket.py
+++ socket.py
@@ -1,5 +1,31 @@
-# import socket
 
-print("Hello, World!")
+#import libraries for server code
+import socket
 
-print(6/7)
+# The port on which to listen
+serverPort = 12000
+
+# Create a TCP socket
+serverSocket = socket (AF INET ,SOCK STREAM) 
+# Bind the socket to the port
+serverSocket.bind (( '' , serverPort )) 
+# Start liste ning for incoming connections
+serverSocket.listen(1)  #This number refers to the number of acceptable connections
+
+print”The server is ready to receive”
+
+# The buffer to store the received data
+data = ""
+
+ # Forever accept incoming connections
+while 1 :
+ # Accept a connection; get client’s socket
+connectionSocket,addr = serverSocket.accept () 
+
+ # Receive whatever the newly connected client has to send
+data = connectionSocket.recv(40)
+print data
+
+
+ # Close the socket
+connection Socket.close()
---
+++ socketClient.py
@@ -0,0 +1,18 @@
+# Client code
+from socket import ∗
+
+# Name and port number of the server to
+# which want to connect .
+serverName = "ecs.fullerton.edu"
+serverPort = 12000 
+# Create a socket
+clientSocket = socket(AF_INET, SOCKSTREAM)
+# Connect to the server
+clientSocket.connect (( serverName , serverPort ))
+# A string we want to send to the server
+data = "Hello world ! This is a very long string."
+# Send that string!
+clientSocket.send(data)
+
+# Close the socket
+clientSocket.close()
