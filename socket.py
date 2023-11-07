1a90061410710804586cadadec5f1ca0bc033cf9
barrales-d <dbarrales06@gmail.com>
2023-11-06 17:27:45 -0800

    Fixed weird typos

--- socket.py
+++ socket.py
@@ -1,18 +1,16 @@
-
 #import libraries for server code
-import socket
-
+from socket import *
 # The port on which to listen
 serverPort = 12000
 
 # Create a TCP socket
-serverSocket = socket (AF INET ,SOCK STREAM) 
+serverSocket = socket(AF_INET , SOCK_STREAM) 
 # Bind the socket to the port
-serverSocket.bind (( '' , serverPort )) 
+serverSocket.bind (( "" , serverPort )) 
 # Start liste ning for incoming connections
 serverSocket.listen(1)  #This number refers to the number of acceptable connections
 
-print”The server is ready to receive”
+print("The server is ready to receive")
 
 # The buffer to store the received data
 data = ""
@@ -20,12 +18,12 @@ data = ""
  # Forever accept incoming connections
 while 1 :
  # Accept a connection; get client’s socket
-connectionSocket,addr = serverSocket.accept () 
+    connectionSocket, addr = serverSocket.accept() 
 
- # Receive whatever the newly connected client has to send
-data = connectionSocket.recv(40)
-print data
+    # Receive whatever the newly connected client has to send
+    data = connectionSocket.recv(40)
+    print(data)
 
 
  # Close the socket
-connection Socket.close()
+connectionSocket.close()
--- socketClient.py
+++ socketClient.py
@@ -1,12 +1,12 @@
 # Client code
-from socket import ∗
+from socket import *
 
 # Name and port number of the server to
 # which want to connect .
 serverName = "ecs.fullerton.edu"
 serverPort = 12000 
 # Create a socket
-clientSocket = socket(AF_INET, SOCKSTREAM)
+clientSocket = socket(AF_INET, socket.SOCKSTREAM)
 # Connect to the server
 clientSocket.connect (( serverName , serverPort ))
 # A string we want to send to the server
