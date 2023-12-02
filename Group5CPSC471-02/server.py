e47bb0d44fad501c59fabb0c11214d479b0b67b7
Tommy Ly <lytommy321@gmail.com>
2023-12-02 14:36:30 -0800

    preparing to turn in

---
+++ Group5CPSC471-02/server.py
@@ -0,0 +1,92 @@
+# import libraries for server code
+import socket
+import datatransfer 
+
+import os
+import sys
+
+# Command line checks
+if len(sys.argv) != 2:
+    print("USEAGE: python3 server.py <PORT NUMBER>")
+    sys.exit(1)
+
+# Getting the port on which to listen
+serverPort = int(sys.argv[1])
+
+# Create a TCP socket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+# Bind the socket to the port
+serverSocket.bind(("", serverPort))
+# Start listening for incoming connections
+serverSocket.listen(1)  # This number refers to the number of acceptable connections
+
+print("The server is ready to receive")
+
+# Forever accept incoming connections
+while True:
+    print("[WAIT] For connections...")
+    # Accept a connection; get client’s socket
+    connectionSocket, addr = serverSocket.accept()
+    print("[CONNECTED] to client:", addr)
+    # Keep the connection with client alive until server recieves `quit` command
+    connected = True
+    while connected:
+        print("[WAIT] For command from client...")
+        commandLength = int(datatransfer.recvData(connectionSocket, 4).decode())
+        command = datatransfer.recvData(connectionSocket, commandLength).decode()
+        print("[INFO] Recieved command:", command)
+
+        succesful = True
+
+        if command == "quit":
+            connected = False
+            break  # breaks out of `while connected: ` loop
+        elif command == "get":
+            fileNameLength = int(datatransfer.recvData(connectionSocket, 10).decode())
+            fileName = datatransfer.recvData(connectionSocket, fileNameLength).decode()
+            print("[INFO] Recieved file name:", fileName)
+
+            if datatransfer.sendFile(connectionSocket, "serverfiles/" + fileName) < 0:
+                succesful = False
+
+        elif command == "put":
+            fileNameLength = int(datatransfer.recvData(connectionSocket, 10).decode())
+            filename = datatransfer.recvData(connectionSocket, fileNameLength).decode()
+
+            fileDataLength = int(datatransfer.recvData(connectionSocket, 10).decode())
+            fileData = datatransfer.recvData(connectionSocket, fileDataLength).decode()    #
+            
+
+            filePath = "serverfiles/" + filename
+            try:
+            # tries to create a file if it DOES NOT exists
+                fo = open(filePath, 'x')
+                fo.write(fileData)
+                fo.close()
+            except FileExistsError:
+            # simply opens the file because it DOES exists 
+                fo = open(filePath, 'w')
+                fo.write(fileData)
+                fo.close()
+
+        elif command == "ls":
+            filenames = os.listdir('serverfiles')
+            print("[INFO]", filenames)
+
+            # TODO: might break for filenames with spaces: `file hello.txt`
+            filenames_string = ' '.join(filenames)
+
+            fileNamesLength = datatransfer.prepareSize(len(filenames_string))
+
+            datatransfer.sendData(connectionSocket, fileNamesLength + filenames_string)
+        
+        if succesful:
+            print("[SUCCESS] Executed:", command)
+        else:
+            print("[FAILURE] Tried to execute:", command)
+    
+    print("[DISCONNECTED] From client:", addr)
+    
+    # Close the socket
+    connectionSocket.close()
---
+++ Group5CPSC471-02/socketClient.py
@@ -0,0 +1,116 @@
+# Client code
+import socket
+import datatransfer
+
+import sys
+
+# Command line checks
+if len(sys.argv) != 3:
+    print("USEAGE: python3 socketClient.py <server machine> <server port>")
+    sys.exit(1)
+
+# Getting port number and the name of the server we want to connect to
+serverAddr = sys.argv[1]
+serverPort = int(sys.argv[2])
+
+# Create a socket
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # Try connecting to the server on the port specified by the client
+    clientSocket.connect((serverAddr, serverPort))
+except ConnectionRefusedError:
+    print(f"Could not connect to {serverAddr}:{serverPort}. The server may not be currently running or the port is wrong.")
+    sys.exit(1)
+
+print("connected to server")
+
+# 
+# Packet Structure:
+#   1. First 4 bytes   = length of command, X
+#   2. Next X bytes    = command
+#   3. Next 10 bytes   = length of next data, Y
+#   4. Next Y bytes    = data
+# Repeat steps 3 and 4 if needed
+# send all data at once through `datatransfer.sendData(sock, packet)`
+# 
+isConnected = True
+while isConnected:
+    userInput = input("ftp > ").split(' ')
+    command = userInput[0]
+    commandLenStr = datatransfer.prepareSize(len(command), 4)
+
+    if command == "quit":
+        # send format: "quit"
+        isConnected = False
+        datatransfer.sendData(clientSocket, commandLenStr + command)
+        break
+    elif command == "get":
+        fileName = ""
+        if len(userInput) < 2 or userInput[1] == "":
+            fileName = input("ftp > Enter File Name: ")
+        else:
+            fileName = userInput[1]
+        
+        fileNamelenStr = datatransfer.prepareSize(len(fileName))
+        # send fileName
+        # sends: "0003get0000000008file.txt"
+        print("Getting " + fileName + " from server")
+        datatransfer.sendData(clientSocket, commandLenStr + command + fileNamelenStr + fileName)
+
+        fileDataLength = int(datatransfer.recvData(clientSocket, 10).decode())
+        fileData = datatransfer.recvData(clientSocket, fileDataLength).decode()
+        # print(file)
+        if fileData == "FAILURE":
+            print("ERROR file not found on server")
+            print("Try command `ls` to see list of files on server")
+            continue
+
+        filePath = "clientfiles/" + fileName
+        print("Saving the file data to `" + filePath + "`")
+        try:
+            # tries to create a file if it DOES NOT exists
+            with open(filePath, 'x') as file:
+                file.write(fileData)
+        except FileExistsError:
+            # simply opens the file because it DOES exists 
+            with open(filePath, 'w') as file:
+                file.write(fileData)
+
+        print(f"Data transfer complete! Filename: {fileName}, Bytes Transferred: {fileDataLength}")
+        
+    elif command == "ls":
+        # send format: "ls"
+        datatransfer.sendData(clientSocket, commandLenStr + command)
+
+        fileNamesLength = int(datatransfer.recvData(clientSocket, 10).decode())
+        fileNames = datatransfer.recvData(clientSocket, fileNamesLength).decode()
+        print("Files in Server:")
+        fileNames = fileNames.split(' ')
+        for file in fileNames:
+            print("   -", file)
+
+    elif command == "put":
+        fileName = ""
+        if len(userInput) < 2 or userInput[1] == "":
+            fileName = input("ftp > Enter File Name: ")
+        else:
+            fileName = userInput[1]
+
+        fileNamelenStr = datatransfer.prepareSize(len(fileName))
+            # send fileName
+            # sends: "0003get0000000008file.txt"
+        print("putting " + fileName + " to server")
+        
+
+        fileNameLength = len(fileName) #added
+        fi = open("clientfiles/"+ fileName, "r") 
+        
+        dataT = fi.read()
+        fileDataLenStr = datatransfer.prepareSize(len(dataT))
+
+        datatransfer.sendData(clientSocket, commandLenStr + command + fileNamelenStr + fileName + fileDataLenStr + dataT)
+        fi.close() #end of previous
+
+# Close the socket
+clientSocket.close()
